0ff8ba6ee895439cbf65fc0bfc154396c79dc552
ales22assist <ales.francisko@assist.cz>
2020-01-06 15:13:09 +0100

    adding updated classes, db finally connected properly

---
+++ src/configuration/ApplicationConf.java
@@ -0,0 +1,58 @@
+package configuration;
+
+import property_file_units.PropertyFileUnits;
+import exception.*;
+
+public class ApplicationConf {
+	
+	private static final String LOG_PROPERTY_FILE = "C:\\dev\\logger\\log.properties";
+	static String propertyStringValue;
+	PropertyFileUnits propertyFileUnits = PropertyFileUnits.getInstance();
+	
+	public static String getDriverClass() {
+		
+			try {
+				return PropertyFileUnits.getInstance().getProperty("DRIVER_CLASS");
+			} catch (PropertyFileException e) {
+				e.printStackTrace();
+				return "";
+			}
+		}
+	
+	public static String getUrl() {
+		try {
+			return PropertyFileUnits.getInstance().getProperty("URL");
+		} catch (PropertyFileException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+	public static String getUserName() {
+		try {
+			return PropertyFileUnits.getInstance().getProperty("USER_NAME");
+		} catch (PropertyFileException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+	public static String getUserPassword() {
+		try {
+			return PropertyFileUnits.getInstance().getProperty("USER_PASSWORD");
+		} catch (PropertyFileException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+	public static String getTabName() {
+		try {
+			return PropertyFileUnits.getInstance().getProperty("TAB_NAME");
+		} catch (PropertyFileException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+	
+	public static String getLogPropertyFile() {
+		return LOG_PROPERTY_FILE;
+	}
+}
--- src/property_file_units/PropertyFileUnits.java
+++ src/property_file_units/PropertyFileUnits.java
@@ -1,5 +1,64 @@
 package property_file_units;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+import configuration.Config;
+import exception.PropertyFileException;
+
+
 public class PropertyFileUnits {
 
+	private static final Logger LOGGER = Logger.getLogger(PropertyFileUnits.class);
+	
+	private static PropertyFileUnits INSTANCE;
+	private final String PROPERTY_FILE = Config.getPropertyFile();
+	private final Properties properties;
+	
+	public PropertyFileUnits() {
+		LOGGER.debug("begin");
+		properties = new Properties();
+		
+		try{
+			FileInputStream fileInputStream = new FileInputStream(PROPERTY_FILE);  
+			properties.load(fileInputStream);
+			fileInputStream.close();
+		} catch (IOException ex){
+			LOGGER.error(ex);
+		}
+	}
+	
+	public static PropertyFileUnits getInstance() {
+		LOGGER.debug("begin");
+		if (INSTANCE == null) {
+			LOGGER.debug("New instance");
+			INSTANCE = new PropertyFileUnits();
+		} else {
+			LOGGER.debug("Old instance");
+		}
+		return INSTANCE;
+	}
+	
+	public static void cancelInstance() {
+		INSTANCE = null;
+	}
+	
+	public String getProperty (String propertyName) throws PropertyFileException {
+		LOGGER.debug("begin");
+		
+		String propertyValue = properties.getProperty(propertyName);
+		LOGGER.debug("propertyName: " + propertyName);
+		LOGGER.debug("propertyValue: " + propertyValue);if (propertyValue == null) {
+			LOGGER.error("Property not found");
+			PropertyFileException propertyFileException = new PropertyFileException("Property not Found");
+			propertyFileException.setPropertyName(propertyName);
+			propertyFileException.setPropertyValue(propertyValue);
+			throw propertyFileException;
+		}
+		return propertyValue;
+	
+	}
 }
